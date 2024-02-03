b94522602fce5e1b5877fe8faff8ba717123834e
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-03 16:30:10 -0600

    newmain.py
    2/3/2024

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#parse month should take in the text of the month and return the number 
-#as a string
-#January -> 1 (as a string)
-#YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
+num_mon = 0
 def parse_month(month):
+    month=month[:3].lower()
     months = {
         'jan': 1,
         'feb': 2,
@@ -17,27 +15,21 @@ def parse_month(month):
          'nov':11,
          'dec':12
         }
-    a = x.strip()[:3].lower()
-    num_month = months[a]
-    return num_month
+
+    return months[month]
 
 extra_zero = 0
-month = 0
 day = 0
-year = 0
-def parse_date(day):
-    if day > 0 and day < 10:
-        day2 = str(day).zfill(extra_zero)
-        print(parse_month(month),'/',day2,'/'year)
-    else:
-        print(parse_month(month),'/',day,'/'year)
+def parse_date(month, day, year):
+    return(parse_month(month),'/',day,'/',year)
         
 if __name__ == '__main__':
-    date = input().strip(' ')
+    date = input().split(' ')
     month = date[0]
     day = date[1]
-    year = date[2]
-    parse_date(day)
+    if day > '0' and day < '10':
+        day = str(day).zfill(extra_zero)
+    day=day[:2]
+    year= date[2]
 
-
-
+    print(parse_date(month,day,year))
