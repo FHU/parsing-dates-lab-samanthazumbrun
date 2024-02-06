61c343534189ed660bdc9cab57d1a4dcd26912f1
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-06 12:41:40 -0600

    main.py
    removed space in my month def/dictionary

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 num_mon = 0
 def parse_month(month):
-    month=month[:3].lower()
+    month = month[:3].lower()
     months = {
         'jan': 1,
         'feb': 2,
         'mar': 3,
         'apr':4,
-         'may':5,
-         'jun':6,
-         'jul':7,
-         'aug':8,
-         'sep':9,
-         'oct':10,
-         'nov':11,
-         'dec':12
+        'may':5,
+        'jun':6,
+        'jul':7,
+        'aug':8,
+        'sep':9,
+        'oct':10,
+        'nov':11,
+        'dec':12
         }
 
     return months[month]
